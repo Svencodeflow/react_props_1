4653be4f54affdfe204ccf6405e1130f124dbaec
Sven Schlaak <123948019+Svencodeflow@users.noreply.github.com>
2023-04-06 14:57:49 +0200

    first

---
+++ src/components/todo.js
@@ -0,0 +1,12 @@
+const Todoes = () => {
+    // console.log(props);
+    return ( 
+        <ul>
+            <li>Einkaufen</li>
+            <li>Auto Fahren</li>
+            <li>Nix Machen</li>
+        </ul>
+     );
+}
+ 
+export default Todoes;
